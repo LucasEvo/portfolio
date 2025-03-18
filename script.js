5d7cfa9d750d71a28cee6ee005d580fdf1fda9b9
LucasEvo <42527886+LucasEvo@users.noreply.github.com>
2025-03-18 01:56:01 -0300

    Botão adicionado - mudança de cor

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.onload = function() {
     alert("Bem-vindo ao meu portfólio!");
 };
+document.getElementById("changeColorBtn").onclick = function() {
+    document.body.style.backgroundColor = "lightblue";
+};
