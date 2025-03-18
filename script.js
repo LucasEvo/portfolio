e236341c06cb591683e78c70142af1a078b07e5d
LucasEvo <42527886+LucasEvo@users.noreply.github.com>
2025-03-18 01:58:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     alert("Bem-vindo ao meu portfólio!");
 };
+
 document.getElementById("changeColorBtn").onclick = function() {
     document.body.style.backgroundColor = "green";
 };
